df9519688c5a376e8067acc1a0090f8c69f91ed6
apollo-dw <83023433+apollo-dw@users.noreply.github.com>
2023-01-25 05:44:30 +0000

    Capitalize site name

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta name="description" content="osu! map database is a platform that allows for the rating of osu! beatmaps."/>
         <meta property="og:title" content="<?php echo $PageTitle; ?> | OMDB">
-        <meta property="og:site_name" content="omdb">
+        <meta property="og:site_name" content="OMDB">
         <meta name="theme-color" content="#2F4F4F">
 		<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
