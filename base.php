e6a44b9472a9397ba207ee75ce39a16e19b9109a
apollo-dw <83023433+apollo-dw@users.noreply.github.com>
2023-01-26 07:10:28 +0000

    Add maintenance disclaimer thing

--- base.php
+++ base.php
@@ -6,4 +6,11 @@
     if ($loggedIn && $user["banned"]) {
         die(".");
     }
+
+    // Should be database'd instead
+    $maintenance = false;
+    if ($maintenance){
+        require("maintenance.php");
+        die("");
+    }
 ?>
---
+++ maintenance.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Maintenance | OMDB</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="osu! map database is a platform that allows for the rating of osu! beatmaps."/>
+        <meta property="og:title" content="OMDB">
+        <meta property="og:site_name" content="OMDB">
+        <meta name="theme-color" content="#2F4F4F">
+        <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="stylesheet" type="text/css" href="/style.css?v=1" />
+        <style>
+            html, body {
+                height: 100%;
+                min-width:80%;
+            }
+
+            html {
+                display: table;
+                margin: auto;
+            }
+
+            body {
+                display: table-cell;
+                vertical-align: middle;
+            }
+        </style>
+    </head>
+    <body>
+        <div style="text-align:center;min-width:80%;">
+            <h1>OMDB is currently undergoing maintenance.</h1>
+            <img src="android-chrome-192x192.png" />
+            <div id="reason">should be a databased reason here but for now i'll edit the live prod of the file</div>
+        </div>
+        <?php
+            include 'footer.php';
+        ?>
+    </body>
