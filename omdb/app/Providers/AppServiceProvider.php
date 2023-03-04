e001b2118c6e658f48093e9aa7037f017f5a90ef
Michael Zhang <mail@mzhang.io>
2023-03-04 14:59:24 -0600

    Format + https

--- omdb/app/Providers/AppServiceProvider.php
+++ omdb/app/Providers/AppServiceProvider.php
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    //
+    if ($this->app->environment("production")) {
+      \URL::forceScheme("https");
+    }
   }
 }
--- omdb/app/View/Components/Ratings/Display.php
+++ omdb/app/View/Components/Ratings/Display.php
@@ -28,10 +28,8 @@ class Display extends Component
     $hint = null;
 
     if ($this->rating->omdb_user) {
-      $custom_ratings = json_decode(
-        $this->rating->omdb_user->custom_ratings,
-        true
-      ) ?? [];
+      $custom_ratings =
+        json_decode($this->rating->omdb_user->custom_ratings, true) ?? [];
       info("custom ratings" . json_encode($custom_ratings));
       $rs = number_format($this->rating->score, 1);
       $hint = $custom_ratings[$rs] ?? null;
