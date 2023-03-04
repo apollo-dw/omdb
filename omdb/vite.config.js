9b0ecdcf3bd65ff2def5f86d814e949c68a9c751
Michael Zhang <mail@mzhang.io>
2023-03-04 00:31:23 -0600

    ohhhh bless Олег Поленин for figuring out how to change the vite url

--- omdb/vite.config.js
+++ omdb/vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         port: 8173,
+        hmr: {
+            host: "192.168.0.133",
+        },
     },
 
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
