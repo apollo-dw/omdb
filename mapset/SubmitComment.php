b935ced88a723a5a50dc26d39167bc0ea172ad50
apollo-dw <83023433+apollo-dw@users.noreply.github.com>
2023-04-02 14:20:37 +0100

    Fix up weird comment behaviour

--- mapset/SubmitComment.php
+++ mapset/SubmitComment.php
@@ -1,50 +1,27 @@
 <?php
-	include '../connection.php';
+    include '../base.php';
 	
 	$setId = $_POST['sID'] ?? -1;
-	$comment = $_POST['comment'] ?? "";
-	if ($beatmapId == -1) {
+	$comment = trim($_POST['comment']) ?? "";
+	if ($setId == -1) {
 		die("NO");
 	}
 	
-	if( strlen($comment) < 5){
+	if( strlen($comment) < 3){
 		die("SHORT");
 	}
 	
-	if( strlen($comment) > 10000){
+	if( strlen($comment) > 8000){
 		die("LONG");
 	}
 	
 	if($conn->query("SELECT * FROM `beatmaps` WHERE `SetID`='${setId}';")->num_rows == 0){
-		die ("NO");
+		die ("NO - Cant Find Map In DB");
 	}
-
-	$loggedIn = false;
-	$userId = -1;
-	$userName = "";
-
-	if (isset($_COOKIE["AccessToken"])) {
-		$token = $_COOKIE["AccessToken"];
-		
-		$stmt = $conn->prepare("SELECT UserID, Username FROM `users` WHERE `AccessToken` = ?");
-		$stmt->bind_param("s", $token);
-		$stmt->execute();
-		$result = $stmt->get_result();
-
-		if ($result->num_rows == 1) {
-			$row = $result->fetch_assoc();
-			$loggedIn = true;
-			$userId = $row['UserID'];
-			$userName = $row['Username'];
-		}
-	}
-
 	
 	if ($loggedIn == false) {
-		die ("NO");
+		die ("NO - Not Logged In");
 	}
-	
-
 	
 	$statement = $conn->prepare("INSERT INTO `comments` (UserID, SetID, Comment) VALUES (?, ?, ?);");
 	$statement->bind_param("sss", $uID, $sID, $cmnt);
