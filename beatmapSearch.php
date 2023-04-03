15e43bb801047fea87ccfd3c10f9853d70b38f09
apollo-dw <83023433+apollo-dw@users.noreply.github.com>
2023-04-03 15:28:20 +0100

    Add user search to search bar

--- beatmapSearch.php
+++ beatmapSearch.php
@@ -2,7 +2,8 @@
 	include_once 'connection.php';
 	
 	$q=$_GET["q"];
-	
+
+    // If it's a link in the query, we should just show the map.
 	if(preg_match('/https:\/\/osu\.ppy\.sh\/(beatmapsets|beatmapset|s)\/(\d+)/', $q, $matches)){
 		$setID = $matches[2];
 		
@@ -12,42 +13,48 @@
 		$res = $stmt->get_result(); 
 		$row = $res->fetch_row();
 		$value = $row ? $row : null;
-		
 		if ($value == null){
 			die("Mapset not found!");
 		}
-		
 		?>
-		
 		<a href="/mapset/<?php echo $setID; ?>"><div style="margin:0;background-color:DarkSlateGrey;" ><?php echo $value[2] . " - " . $value[1]; ?></div></a>
-		
 		<?php
-		
 		die();
 	}
-	
-	// This shit straight from chatgpt
-	// Prepare the statement
+
+    $userStmt = $conn->prepare("SELECT `UserID`, `Username` FROM mappernames WHERE username LIKE ? LIMIT 5;");
+    $like = "%$q%";
+    $userStmt->bind_param("s", $like);
+    $userStmt->execute();
+    $userStmt->bind_result($userID, $username);
+    $userStmt->store_result();
+
+    if ($userStmt->num_rows > 0){
+        echo "<div style='background-color:#182828;><b>Users</b></div>";
+        $counter = 0;
+        while ($userStmt->fetch()) {
+            $counter += 1;
+            ?>
+            <a href="/profile/<?php echo $userID; ?>"><div style="<?php if ($counter % 2 == 0){ echo 'background-color:DarkSlateGrey;'; } else { echo 'background-color:#203838;'; } ?>padding:0.25em;display:flex;vertical-align: middle" ><img src="https://s.ppy.sh/a/<?php echo $userID; ?>" style="height:24px;width:24px;" title="<?php echo $username; ?>"/> <?php echo $username; ?></div> </a>
+            <?php
+        }
+    }
+
 	$stmt = $conn->prepare("SELECT `SetID`, Title, Artist, DifficultyName FROM `beatmaps` WHERE MATCH (DifficultyName, Artist, Title) AGAINST(? IN NATURAL LANGUAGE MODE) AND Mode='0' LIMIT 25;");
-
-	// Bind the parameters
-	$like = "%$q%";
 	$stmt->bind_param("s", $like);
-
-	// Execute the statement
 	$stmt->execute();
-
-	// Bind the result variables
 	$stmt->bind_result($setId, $title, $artist, $difficultyName);
+    $stmt->store_result();
 
-	// Fetch the results
-	$counter = 0;
-	while ($stmt->fetch()) {
-	$counter += 1;
-	?>
-<a href="/mapset/<?php echo $setId; ?>"><div style="<?php if ($counter % 2 == 0){ echo 'background-color:DarkSlateGrey;'; } else { echo 'background-color:#203838;'; } ?>margin:0;" ><?php echo $artist . " - " . $title . " [" . $difficultyName . "]"; ?></div></a>
-	<?php
-	}
-?>
+    if ($stmt->num_rows > 0){
+        $counter = 0;
+        echo "<div style='background-color:#182828;'><b>Maps</b></div>";
+        while ($stmt->fetch()) {
+            $counter += 1;
+            ?>
+            <a href="/mapset/<?php echo $setId; ?>"><div style="<?php if ($counter % 2 == 0){ echo 'background-color:DarkSlateGrey;'; } else { echo 'background-color:#203838;'; } ?>margin:0;" ><?php echo $artist . " - " . $title . " [" . $difficultyName . "]"; ?></div></a>
+            <?php
+        }
+    }
 
 
