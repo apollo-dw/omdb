719c6b316e4119764e76a16f5fe0eec0d61f202e
js1086 <js1086@student.le.ac.uk>
2023-06-04 19:20:42 +0100

    Actually use the correct variable

--- profile/index.php
+++ profile/index.php
@@ -23,7 +23,7 @@
             $ratingCounts[$row['Score']] = $row['count'];
         }
 
-        $maxRating = sizeof($arr) >= 1 ? max($ratingCounts) : 1;
+        $maxRating = sizeof($ratingCounts) >= 1 ? max($ratingCounts) : 1;
 
         $mutuals = $conn->query("SELECT u.UserID as ID, u.Username as username FROM users u
                                        JOIN user_relations ur1 ON u.UserID = ur1.UserIDTo
