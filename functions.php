ae6ba9f3642310208bf297e22f3d7342b9802a92
js1086 <js1086@student.le.ac.uk>
2023-06-24 16:10:09 +0100

    Shorten time display

--- functions.php
+++ functions.php
@@ -127,24 +127,24 @@
 		$diff->d -= $diff->w * 7;
 
 		$string = array(
-			'y' => 'year',
-			'm' => 'month',
-			'w' => 'week',
-			'd' => 'day',
-			'h' => 'hour',
-			'i' => 'minute',
-			's' => 'second',
+			'y' => 'Y',
+			'm' => 'M',
+			'w' => 'w',
+			'd' => 'd',
+			'h' => 'h',
+			'i' => 'm',
+			's' => 's',
 		);
 		foreach ($string as $k => &$v) {
 			if ($diff->$k) {
-				$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+				$v = $diff->$k . $v;
 			} else {
 				unset($string[$k]);
 			}
 		}
 
 		if (!$full) $string = array_slice($string, 0, 1);
-		return $string ? implode(', ', $string) . ' ago' : 'just now';
+		return $string ? implode(', ', $string) . ' ago' : 'now';
 	}
 
 	function GetUserNameFromId($id, $conn){
