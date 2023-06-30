d55fadced4e42bb012e52514fa0821f38bfa9d3c
js1086 <js1086@student.le.ac.uk>
2023-07-01 00:50:50 +0100

    Fetch scores from currently selected mode

--- dashboard/FetchScores.php
+++ dashboard/FetchScores.php
@@ -1,13 +1,27 @@
 <?php
 	require "../base.php";
 	header('Content-Type: application/json; charset=utf-8');
-	ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 	$curl = curl_init();
-	
+
+    $modeString = "osu";
+    switch($mode){
+        case 0:
+            $modeString = "osu";
+            break;
+        case 1:
+            $modeString = "taiko";
+            break;
+        case 2:
+            $modeString = "fruits";
+            break;
+        case 3:
+            $modeString = "mania";
+            break;
+    }
+
 	curl_setopt_array($curl, array(
-	  CURLOPT_URL => 'https://osu.ppy.sh/api/v2/users/' . $userId . '/scores/recent?mode=osu&limit=10',
+	  CURLOPT_URL => 'https://osu.ppy.sh/api/v2/users/' . $userId . '/scores/recent?mode=' . $modeString . '&limit=10',
 	  CURLOPT_HTTPHEADER => ['Accept: application/json', 'Content-Type: application/json', 'Authorization: Bearer ' . $user['AccessToken']],
 	  CURLOPT_RETURNTRANSFER => true,
 	  CURLOPT_ENCODING => '',
@@ -20,20 +34,19 @@ error_reporting(E_ALL);
 
 	$response = curl_exec($curl);
 	curl_close($curl);
-	
+
 	$scores = json_decode($response);
-	
+
 	$stmt = $conn->prepare("SELECT `Score` FROM `ratings` WHERE `BeatmapID`=? AND `UserID`=?;");
 	$stmt->bind_param("si", $bID, $userId);
-	
+
 	foreach ($scores as &$score) {
 		$bID = $score->beatmap->id;
 		$stmt->execute();
 		$result = $stmt->get_result()->fetch_row()[0] ?? "-1";
 		$score->rating = $result;
 	}
-	
+
 	$response = json_encode(array_reverse($scores));
-	
+
 	echo $response;
-	
