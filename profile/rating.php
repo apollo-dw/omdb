c8d2ee40c05a730416fac8e6800fd2fb44364abb
js1086 <js1086@student.le.ac.uk>
2023-07-17 13:51:38 +0100

    Add tags to profile latest ratings display

--- profile/rating.php
+++ profile/rating.php
@@ -1,41 +1,33 @@
 <?php
 	include_once '../connection.php';
 	include_once '../functions.php';
-	
-	$choice = $_GET['c'] ?? -1;
-	
-	if (isset($_GET['p'])){
-		$profileId = $_GET['p'];
-	}
 
-	$starString = "";
-	if ($choice != -1){
-		$starString = "AND `Score`='{$choice}'";
-	}
-
-    $stmt = $conn->prepare("SELECT r.*, b.*
-                            FROM `ratings` r
-                            JOIN `beatmaps` b ON r.`BeatmapID` = b.`BeatmapID`
-                            WHERE r.`UserID` = ? AND b.`Mode` = ?
-                            {$starString}
-                            ORDER BY r.`date` DESC
-                            LIMIT 50");
+$stmt = $conn->prepare("SELECT r.*, b.*, GROUP_CONCAT(t.Tag SEPARATOR ', ') AS Tags
+                        FROM `ratings` r
+                        JOIN `beatmaps` b ON r.`BeatmapID` = b.`BeatmapID`
+                        LEFT JOIN `rating_tags` t ON t.`BeatmapID` = b.`BeatmapID` AND t.`UserID` = r.`UserID`
+                        WHERE r.`UserID` = ? AND b.`Mode` = ?
+                        GROUP BY b.`BeatmapID`, r.`date`
+                        ORDER BY r.`date` DESC
+                        LIMIT 50");
     $stmt->bind_param("ii", $profileId, $mode);
     $stmt->execute();
     $result = $stmt->get_result();
 
-    while ($row = $result->fetch_assoc()) {
-        $beatmap = $row;
+    while ($beatmap = $result->fetch_assoc()) {
+        $tags = htmlspecialchars($beatmap['Tags'], ENT_COMPAT, "ISO-8859-1")
 ?>
 	<div class="flex-container ratingContainer alternating-bg">
 		<div class="flex-child">
 			<a href="/mapset/<?php echo $beatmap["SetID"]; ?>"><img src="https://b.ppy.sh/thumb/<?php echo $beatmap['SetID']; ?>l.jpg" class="diffThumb"/ onerror="this.onerror=null; this.src='../charts/INF.png';"></a>
 		</div>
-		<div class="flex-child" style="flex:0 0 70%;">
-			<?php echo RenderUserRating($conn, $row); ?> on <a href="/mapset/<?php echo $beatmap["SetID"]; ?>"><?php echo htmlspecialchars("{$beatmap["Artist"]} - {$beatmap["Title"]} [{$beatmap["DifficultyName"]}]");?></a>
-		</div>
-		<div class="flex-child" style="width:100%;">
-			<?php echo GetHumanTime($row["date"]); ?>
+		<div class="flex-child" style="flex:0 0 85%;">
+			<?php echo RenderUserRating($conn, $beatmap); ?> on <a href="/mapset/<?php echo $beatmap["SetID"]; ?>"><?php echo mb_strimwidth(htmlspecialchars("{$beatmap["Title"]} [{$beatmap["DifficultyName"]}]"), 0, 80, "..."); ?></a>
+		    <br>
+            <span class="subText"><?php echo $tags; ?></span>
+        </div>
+		<div class="flex-child" style="margin-left:auto;">
+			<?php echo GetHumanTime($beatmap["date"]); ?>
 		</div>
 	</div>
 <?php
