8d79b0ec77667066ef05f1cacfb7e471499ba528
js1086 <js1086@student.le.ac.uk>
2023-07-21 00:52:55 +0100

    Add open beatmap edit requests to admin

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,13 @@
 <?php
     include 'header.php';
-    ?>
 
-test
+    $stmt = $conn->prepare("SELECT e.*, b.SetID, b.Title, b.DifficultyName FROM beatmap_edit_requests e JOIN beatmaps b on e.BeatmapID = b.BeatmapID WHERE e.Status = 'Pending' ORDER BY e.`Timestamp`;");
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    echo "<b>Open edit requests:</b><br>";
+    while($row = $result->fetch_assoc()) {
+        $name = GetUserNameFromId($row["UserID"], $conn);
+        echo "<a href='../mapset/?mapset_id={$row["SetID"]}'>{$name} on {$row["Title"]} [{$row["DifficultyName"]}]</a> ({$row["Timestamp"]})<br>";
+    }
+
