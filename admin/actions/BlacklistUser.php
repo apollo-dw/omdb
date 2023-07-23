9f4e15ceac327432bcaecc0c620688a385d2994c
js1086 <js1086@student.le.ac.uk>
2023-07-23 17:12:00 +0100

    Update blacklist script to cover collab cases

--- admin/actions/BlacklistUser.php
+++ admin/actions/BlacklistUser.php
@@ -8,18 +8,27 @@
     $stmt->bind_param("s", $blacklistUserID);
     $stmt->execute();
 
-    $stmt = $conn->prepare("UPDATE beatmaps
-                           SET ChartRank = NULL,
-                               ChartYearRank = NULL,
-                               Rating = NULL,
-                               Blacklisted = '1',
-                               BlacklistReason = 'mapper has requested blacklist'
-                           WHERE BeatmapID IN (
-                               SELECT DISTINCT bc.BeatmapID
-                               FROM beatmap_creators bc
-                               JOIN blacklist b ON bc.CreatorID = b.UserID
-                               WHERE bc.CreatorID = ?
-                           )");
-
-    $stmt->bind_param("s", $blacklistUserID);
+    $stmt = $conn->prepare("UPDATE beatmaps AS bm
+                                  SET ChartRank = NULL,
+                                    ChartYearRank = NULL,
+                                    Rating = NULL,
+                                    Blacklisted = '1',
+                                    BlacklistReason = 'mapper has requested blacklist'
+                                  WHERE bm.BeatmapID IN (
+                                    SELECT bc1.BeatmapID
+                                    FROM beatmap_creators bc1
+                                    WHERE bc1.CreatorID IN (
+                                        SELECT UserID
+                                        FROM blacklist
+                                    )
+                                    AND NOT EXISTS (
+                                        SELECT 1
+                                        FROM beatmap_creators bc2
+                                        WHERE bc1.BeatmapID = bc2.BeatmapID
+                                        AND bc2.CreatorID NOT IN (
+                                            SELECT UserID
+                                            FROM blacklist
+                                        )
+                                    )
+                                  );");
     $stmt->execute();
