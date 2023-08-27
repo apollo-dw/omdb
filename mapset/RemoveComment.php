6b2161395ccf9eb6523da5c45c44f74a03faf56e
js1086 <js1086@student.le.ac.uk>
2023-08-27 18:23:29 +0100

    Log removed comments

--- mapset/RemoveComment.php
+++ mapset/RemoveComment.php
@@ -15,6 +15,28 @@
         die("NO");
     }
 
+    $stmt = $conn->prepare("SELECT * FROM `comments` WHERE `CommentID` = ? and `SetID` = ?;");
+    $stmt->bind_param("ii", $commentId, $setId);
+    $stmt->execute();
+    $result = $stmt->get_result()->fetch_assoc();
+
+    $array = array(
+        "type" => "comment_deletion",
+        "data" => array(
+            "CommentID" => $result["CommentID"],
+            "UserID" => $result["UserID"],
+            "SetID" => $result["SetID"],
+            "Comment" => $result["Comment"],
+            "Date" => $result["date"],
+        ));
+
+    $json = json_encode($array);
+
+    $stmt = $conn->prepare("INSERT INTO logs (UserID, LogData) VALUES (?, ?);");
+    $stmt->bind_param("is", $userId, $json);
+    $stmt->execute();
+    $stmt->close();
+
     $stmt = $conn->prepare("DELETE FROM `comments` WHERE `CommentID` = ? AND `UserID` = ? AND `SetID` = ?");
     $stmt->bind_param("iii", $commentId, $userId, $setId);
     $stmt->execute();
