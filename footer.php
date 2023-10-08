16e3a5123cebc0566185ac9c70fa6799e41351f9
js1086 <js1086@student.le.ac.uk>
2023-10-08 22:14:53 +0100

    fix

--- footer.php
+++ footer.php
@@ -4,13 +4,11 @@
 		
 		</div>
 		<div class="footerBar">
-			omdb made by <a href="https://omdb.nyahh.net/profile/9558549">apollo</a> |
-            icon made by <a href="https://omdb.nyahh.net/profile/7081160">olc</a> |
             <a href="/rules/">rules</a> |
             <a href="https://github.com/apollo-dw/omdb">github</a> |
             <a href="https://discord.gg/NwcphppBMG">discord</a> |
-            <a href="/descriptors">descriptors</a><br> |
-            <a href="/edit-queue">edit queue</a><br> |
+            <a href="/descriptors">descriptors</a>|
+            <a href="/edit-queue">edit queue</a> |
             <a href="/project-legacy">project legacy</a><br>
             <span style="color:black;"><?php echo $timeForPageExecution; ?>s</span>
 		</div>
