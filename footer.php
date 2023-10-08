40859eaf53de955dabd2f284e8dd64b18faad5b2
js1086 <js1086@student.le.ac.uk>
2023-10-08 22:14:17 +0100

    Add more stuff to footer

--- footer.php
+++ footer.php
@@ -9,7 +9,9 @@
             <a href="/rules/">rules</a> |
             <a href="https://github.com/apollo-dw/omdb">github</a> |
             <a href="https://discord.gg/NwcphppBMG">discord</a> |
-            <a href="/edit-queue">edit queue</a><br>
+            <a href="/descriptors">descriptors</a><br> |
+            <a href="/edit-queue">edit queue</a><br> |
+            <a href="/project-legacy">project legacy</a><br>
             <span style="color:black;"><?php echo $timeForPageExecution; ?>s</span>
 		</div>
     </body>
