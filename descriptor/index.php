5619d82cf53302cd1eae315de3ca6a4457afcac1
js1086 <js1086@student.le.ac.uk>
2023-11-05 02:42:40 +0000

    Add descriptor usage graph

--- descriptor/index.php
+++ descriptor/index.php
@@ -33,18 +33,47 @@
     echo $descriptor["ShortDescription"] . "<br>";
     echo "<span class='subText'>$parentTree</span>";
 ?>
+
+<style>
+    .ratingDistributionContainer {
+        width: calc(100% / 17);
+        height: 6em;
+        margin: 0;
+        color: rgba(125, 125, 125, 0.66);
+        vertical-align: bottom;
+        white-space: nowrap;
+        box-sizing: content-box;
+        padding-top: 2em;
+    }
+
+    .ratingDistributionContainer > .bar {
+        width: 100%;
+        text-align: left;
+        display: inline-block;
+        vertical-align: bottom;
+        background-color: rgba(125, 125, 125, 0.66);
+        border-bottom: 2px solid rgba(125, 125, 125, 0.66);
+        box-sizing: border-box;
+        margin: 0;
+    }
+
+    .bar > span {
+        position: relative;
+        top: 100%;
+    }
+</style>
+
 <br><br>
 <a href="../descriptors/">View all descriptors</a>
 <br><br>
 
 <h2 style="margin-bottom: 0px;">Highest ranked <?php echo $descriptor["Name"]; ?> maps</h2><br>
-<div class="flex-container" style="width:100%;background-color:DarkSlateGrey;padding:0px;">
-    <br>
+<div class="flex-container alternating-bg" style="width:100%;padding:0;margin-bottom:2em;">
     <?php
     $stmt = $conn->prepare("WITH RECURSIVE DescendantDescriptors AS (
                                     SELECT DescriptorID, ParentID
                                     FROM descriptors
-                                    WHERE DescriptorID = ?  -- Specify the starting DescriptorID
+                                    WHERE DescriptorID = ?
                                     UNION ALL
                                     SELECT d.DescriptorID, d.ParentID
                                     FROM descriptors d
@@ -77,6 +106,52 @@
 
     $stmt->close();
     ?>
+</div>
+
+<h2 style="margin-bottom: 0px;">Years active</h2><br>
+<div class="alternating-bg" style="width:100%;padding:0px;">
+    <div class="ratingDistributionContainer">
+        <?php
+        $stmt = $conn->prepare("WITH RECURSIVE DescendantDescriptors AS (
+                                        SELECT DescriptorID, ParentID
+                                        FROM descriptors
+                                        WHERE DescriptorID = ?
+                                        UNION ALL
+                                        SELECT d.DescriptorID, d.ParentID
+                                        FROM descriptors d
+                                        JOIN DescendantDescriptors dd ON d.ParentID = dd.DescriptorID
+                                    )
+                                    SELECT YEAR(b.DateRanked) AS Year, COUNT(b.BeatmapID) AS BeatmapCount
+                                    FROM beatmaps b
+                                    JOIN descriptor_votes dv ON b.BeatmapID = dv.BeatmapID
+                                    JOIN DescendantDescriptors dd ON dv.DescriptorID = dd.DescriptorID
+                                    WHERE b.Mode = ?
+                                    GROUP BY Year
+                                    ORDER BY Year;");
+        $stmt->bind_param("ii", $descriptor_id, $mode);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $currentYear = date("Y");
+        $yearlyDescriptorCounts = array();
+        for ($year = 2007; $year <= $currentYear; $year++) {
+            $yearlyDescriptorCounts[$year] = 0;
+        }
+
+        $maxCount = 0;
+        while ($row = $result->fetch_assoc()) {
+            $yearlyDescriptorCounts[$row['Year']] = $row['BeatmapCount'];
+
+            if ($maxCount < $row['BeatmapCount'])
+                $maxCount = $row['BeatmapCount'];
+        }
+
+        foreach ($yearlyDescriptorCounts as $year => $count) {
+            $proportion = ($count/$maxCount) * 100;
+            echo "<div class='bar' style='height: {$proportion}%;'><span>{$year}</span></div>";
+        }
+        ?>
+    </div>
 </div>
 
 <?php
