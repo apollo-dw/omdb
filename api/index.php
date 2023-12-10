d602defed930b298b92a3c9cbce838548af11aa0
js1086 <js1086@student.le.ac.uk>
2023-12-10 16:19:06 +0000

    Enable CORS headers

--- api/index.php
+++ api/index.php
@@ -1,5 +1,8 @@
 <?php
     require '../base.php';
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
     header('Content-Type: application/json; charset=utf-8');
 
     $response = array();
