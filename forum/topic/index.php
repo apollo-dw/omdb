7858488516f80de357fde8cd775d16ad260200d1
js1086 <js1086@student.le.ac.uk>
2023-12-11 20:53:22 +0000

    Add pagination to... topics. I meant posts the first time

--- forum/topic/index.php
+++ forum/topic/index.php
@@ -1,5 +1,6 @@
 <?php
     $topicId = $_GET['id'] ?? -1;
+    $page = $_GET['p'] ?? 1;
     require_once "../../base.php";
 
     $stmt = $conn->prepare("SELECT * FROM forum_topics WHERE TopicID = ?;");
@@ -8,11 +9,28 @@
     $topic = $stmt->get_result()->fetch_assoc();
     $stmt->close();
 
-    if (is_null($topic) || !is_numeric($topicId))
+    if (is_null($topic) || !is_numeric($topicId) || !is_numeric($page))
         die("ahhh");
 
     $PageTitle = $topic["Name"];
     require_once '../../header.php';
+
+    $limit = 50;
+    $stmt = $conn->prepare("SELECT COUNT(*) FROM forum_threads WHERE TopicID = ?;");
+    $stmt->bind_param("i", $topicId);
+    $stmt->execute();
+    $stmt->bind_result($count);
+    $stmt->fetch();
+    $stmt->close();
+    $pageCount = floor($count / $limit) + 1;
+    $prevPage = max($page - 1, 1);
+    $nextPage = min($page + 1, $pageCount);
+    $pageString = "LIMIT {$limit}";
+
+    if ($page > 1) {
+        $lower = ($page - 1) * $limit;
+        $pageString = "LIMIT {$lower}, {$limit}";
+    }
 
     $stmt = $conn->prepare("SELECT
     ft.ThreadID,
@@ -29,37 +47,37 @@
     LEFT JOIN forum_posts fp ON ft.ThreadID = fp.ThreadID
     WHERE ft.TopicID = ?
     GROUP BY ft.ThreadID
-    ORDER BY LatestPostCreatedAt DESC;");
+    ORDER BY LatestPostCreatedAt DESC {$pageString};");
 
     $stmt->bind_param("i", $topicId);
     $stmt->execute();
     $threads = $stmt->get_result();
     $stmt->close();
 ?>
-    <style>
-        h2 {
-            margin: 0;
-            display: inline;
-        }
 
-        .forum-thread {
-            width: 100%;
-            height: 6em;
-            box-sizing: border-box;
-            align-items: center;
-            padding: 0.5em;
-        }
-    </style>
+<style>
+    h2 {
+        margin: 0;
+        display: inline;
+    }
 
-    <h2><?php echo $topic["Name"]; ?></h2>
-    <div>
-        <span class="subText"><?php echo $topic["Description"]; ?></span>
-        <?php if ($loggedIn) { ?>
-            <div style="float:right;"><a href="../new/?id=<?php echo $topicId; ?>">Create new post</a></div>
-        <?php } ?>
-    </div>
+    .forum-thread {
+        width: 100%;
+        height: 6em;
+        box-sizing: border-box;
+        align-items: center;
+        padding: 0.5em;
+    }
+</style>
 
-    <hr>
+<h2><?php echo $topic["Name"]; ?></h2>
+<div>
+    <span class="subText"><?php echo $topic["Description"]; ?></span>
+    <?php if ($loggedIn) { ?>
+        <div style="float:right;"><a href="../new/?id=<?php echo $topicId; ?>">Create new post</a></div>
+    <?php } ?>
+</div>
+<hr>
 
 <?php
     while($thread = $threads->fetch_assoc()) {
@@ -94,6 +112,16 @@
         <?php
     }
 ?>
+
+<div style="float:right;">
+    <div class="pagination">
+        <a href="<?php echo "?id={$topicId}&p={$prevPage}"; ?>"><span>&laquo;</span></a>
+        <?php for ($i = 1; $i <= $pageCount; $i++) { ?>
+            <a href="<?php echo "?id={$topicId}&p={$i}"; ?>"><span class="pageLink <?php if ($page == $i) echo 'active' ?>"><?php echo $i ?></span></a>
+        <?php } ?>
+        <a href="<?php echo "?id={$topicId}&p={$nextPage}"; ?>"><span>&raquo;</span></a>
+    </div>
+</div>
 
 <?php
     require ('../../footer.php');
