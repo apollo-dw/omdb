b87e13bc6f5246edf65af606803df74a4a70b729
Apollo <js1086@student.le.ac.uk>
2026-05-26 16:39:28 +0100

    Increment version counts

--- header.php
+++ header.php
@@ -22,8 +22,8 @@ error_reporting(E_ALL);
 		<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 		<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 		<link rel="manifest" href="/site.webmanifest">
-		<link rel="stylesheet" type="text/css" href="/style.css?v=33" />
-        <script src="/script.js?v=3"></script>
+		<link rel="stylesheet" type="text/css" href="/style.css?v=34" />
+        <script src="/script.js?v=4"></script>
 	</head>
 	<body>
 		<div class="topBar">
