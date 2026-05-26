ccdb0a7c266e7dd4b9e103d1886365145ef24e89
Apollo <js1086@student.le.ac.uk>
2026-05-26 15:00:44 +0100

    Add tooltip support

--- script.js
+++ script.js
@@ -1,67 +1,86 @@
-$(document).ready(function() {
-    $('#osuLink').on('click', function() {
-        setGameMode(0)
-    });
+$(document).ready(function () {
+  $("#osuLink").on("click", function () {
+    setGameMode(0);
+  });
 
-    $('#taikoLink').on('click', function() {
-        setGameMode(1)
-    });
+  $("#taikoLink").on("click", function () {
+    setGameMode(1);
+  });
 
-    $('#catchLink').on('click', function() {
-        setGameMode(2)
-    });
+  $("#catchLink").on("click", function () {
+    setGameMode(2);
+  });
 
-    $('#maniaLink').on('click', function() {
-        setGameMode(3)
-    });
+  $("#maniaLink").on("click", function () {
+    setGameMode(3);
+  });
 });
 
 function setGameMode(mode) {
-    var expirationDate = new Date();
-    expirationDate.setFullYear(expirationDate.getFullYear() + 1);
+  var expirationDate = new Date();
+  expirationDate.setFullYear(expirationDate.getFullYear() + 1);
 
-    var cookieValue = "mode=" + mode + "; expires=" + expirationDate.toUTCString() + ";path=/;";
-    document.cookie = cookieValue;
-    location.reload();
+  var cookieValue =
+    "mode=" + mode + "; expires=" + expirationDate.toUTCString() + ";path=/;";
+  document.cookie = cookieValue;
+  location.reload();
 }
 
 let debounceTimer;
 
 function showResult(str) {
-    if (str.length == 0) {
-        document.getElementById("topBarSearchResults").innerHTML = "";
-        document.getElementById("topBarSearchResults").style.display = "none";
-        return;
-    }
+  if (str.length == 0) {
+    document.getElementById("topBarSearchResults").innerHTML = "";
+    document.getElementById("topBarSearchResults").style.display = "none";
+    return;
+  }
 
-    clearTimeout(debounceTimer);
+  clearTimeout(debounceTimer);
 
-    debounceTimer = setTimeout(function () {
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("topBarSearchResults").innerHTML = this.responseText;
-                document.getElementById("topBarSearchResults").style.display = "block";
-            }
-        };
-        xmlhttp.open("GET", "/beatmapSearch.php?q=" + str, true);
-        xmlhttp.send();
-    }, 300);
+  debounceTimer = setTimeout(function () {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("topBarSearchResults").innerHTML =
+          this.responseText;
+        document.getElementById("topBarSearchResults").style.display = "block";
+      }
+    };
+    xmlhttp.open("GET", "/beatmapSearch.php?q=" + str, true);
+    xmlhttp.send();
+  }, 300);
 }
 
 function searchFocus() {
-    document.getElementById("topBarSearchResults").style.display="block";
+  document.getElementById("topBarSearchResults").style.display = "block";
 }
 
 function openTab(name) {
-    let x = document.getElementsByClassName("tab");
-    for (let i = 0; i < x.length; i++)
-        x[i].style.display = "none";
+  let x = document.getElementsByClassName("tab");
+  for (let i = 0; i < x.length; i++) x[i].style.display = "none";
 
-    let buttons = document.getElementsByClassName("tabbed-container-nav")[0].getElementsByTagName("button");
-    for (let i = 0; i < buttons.length; i++)
-        buttons[i].classList.remove("active");
+  let buttons = document
+    .getElementsByClassName("tabbed-container-nav")[0]
+    .getElementsByTagName("button");
+  for (let i = 0; i < buttons.length; i++)
+    buttons[i].classList.remove("active");
 
-    document.getElementById(name).style.display = "block";
-    event.target.classList.add("active");
+  document.getElementById(name).style.display = "block";
+  event.target.classList.add("active");
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".tooltip-wrapper").forEach((wrapper) => {
+    const tooltip = wrapper.querySelector(".tooltip-box");
+
+    wrapper.addEventListener("mouseenter", () => {
+      tooltip.classList.remove("flip");
+
+      const rect = tooltip.getBoundingClientRect();
+
+      if (rect.top < 10) {
+        tooltip.classList.add("flip");
+      }
+    });
+  });
+});
