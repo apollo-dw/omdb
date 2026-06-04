bf7e8dc1bb5ad93fcedd9e74a08ffc438dc8b0ad
Apollo <js1086@student.le.ac.uk>
2026-06-04 19:48:49 +0100

    Add Discord link

--- footer.php
+++ footer.php
@@ -4,10 +4,11 @@
 		
 		</div>
 		<div class="footerBar">
+			<a href="https://discord.gg/PWVGrQRq2w" target="_blank">discord</a> |
             <a href="https://github.com/apollo-dw/omdb/" target="_blank">github</a> |
             <a href="/rules/">rules</a> |
             <a href="/descriptors">descriptors</a> |
-            <a href="/edit-queue">edit queue</a>
+            <a href="/edit-queue">edit queue</a> <br>
             <span style="color:black;"><?php echo $timeForPageExecution; ?>s</span>
 		</div>
     </body>
