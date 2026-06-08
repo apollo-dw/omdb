fef9a7901d9c21d06baca71a0c9e31e8cde29f89
VINXIS <oykxf2@gmail.com>
2026-06-07 18:21:19 -0600

    use apiv2 func and abstract set params outside of diff loop

--- maps/add/AddGravedMapset.php
+++ maps/add/AddGravedMapset.php
@@ -20,23 +20,7 @@
         die("No");
     }
 
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://osu.ppy.sh/api/get_beatmaps?k=${apiV1Key}&s=${requestedSetId}",
-        CURLOPT_HTTPHEADER => ['Accept: application/json', 'Content-Type: application/json'],
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-    ));
-
-    $response = curl_exec($curl);
-    curl_close($curl);
-
-    $array = json_decode($response, true);
+    $set = GetBeatmapsetDataOsuApi($token, $requestedSetId);
 
     $beatmap_stmt = $conn->prepare("INSERT INTO `beatmaps` (BeatmapID, SetID, SR, DifficultyName, Mode, Status, Blacklisted, BlacklistReason, Timestamp)
         VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);");
@@ -46,19 +30,58 @@
     $beatmapset_stmt->bind_param("ssiiiisiii", $dateRanked, $artist, $setID, $creatorID, $genre, $lang, $title, $status, $hasStoryboard, $hasVideo);
 
     $creators_stmt = $conn->prepare("INSERT INTO beatmap_creators (BeatmapID, CreatorID) VALUES (?, ?)");
-    $creators_stmt->bind_param("ii", $beatmapID, $creatorID);
+    $creators_stmt->bind_param("ii", $beatmapID, $diffCreatorID);
 
-    if (sizeof($array) == 0) {
+    if (!$set || sizeof($set["beatmaps"] ?? []) == 0) {
         die("there are no maps found from this id (did u paste in beatmap id)");
     }
 
-    foreach($array as $diff){
-        if($diff["approved"] != -2){
+    // Set-based params
+    $artist = $set["artist"];
+    $creatorID = $set["user_id"];
+    $setID = $set["id"];
+    $genre = $set["genre"]["id"];
+    $lang = $set["language"]["id"];
+    $title = $set["title"];
+    $status = $set["ranked"];
+    $blacklisted = 0;
+    $hasStoryboard = (int)$set["storyboard"];
+    $hasVideo = (int)$set["video"];
+    $dateRanked = date("Y-m-d", strtotime($set["last_updated"]));
+
+    // Blacklist + last update checks
+    $query2 = $conn->prepare("SELECT * FROM blacklist WHERE UserID = ?");
+    $query2->bind_param("i", $creatorID);
+    $query2->execute();
+    $query2->store_result();
+    if ($query2->num_rows > 0) {
+        $query2->close();
+        die("No");
+    }
+    $query2->close();
+
+    
+    if (strtotime($set["last_updated"]) > strtotime("-6 months")) {
+        die("No - not old enough");
+    }
+
+    $query3 = $conn->prepare("SELECT * FROM beatmapsets WHERE SetID = ?");
+    $query3->bind_param("i", $setID);
+    $query3->execute();
+    $query3->store_result();
+    if ($query3->num_rows == 0) {
+        $beatmapset_stmt->execute();
+    }
+    $query3->close();
+
+    // Diff-based params
+    foreach($set["beatmaps"] as $diff){
+        if($diff["ranked"] != -2){
             continue;
         }
 
         $query1 = $conn->prepare("SELECT * FROM `beatmaps` WHERE `BeatmapID` = ?");
-        $query1->bind_param("i", $diff["beatmap_id"]);
+        $query1->bind_param("i", $diff["id"]);
         $query1->execute();
         $query1->store_result();
         if ($query1->num_rows > 0) {
@@ -67,48 +90,19 @@
         }
         $query1->close();
 
-        $currentTimestamp = time();
-        $sixMonthsAgo = strtotime("-6 months", $currentTimestamp);
-        if (strtotime($diff["last_update"]) > $sixMonthsAgo) {
-            die("No - not old enough");
-        }
-
-        $dateRanked = date("Y-m-d", strtotime($diff["last_update"]));
-        $artist = $diff["artist"];
-        $beatmapID = $diff["beatmap_id"];
-        $creatorID = $diff["creator_id"];
-        $setID = $diff["beatmapset_id"];
-        $SR = $diff["difficultyrating"];
-        $genre = $diff["genre_id"];
-        $lang = $diff["language_id"];
-        $title = $diff["title"];
+        $dateRanked = date("Y-m-d", strtotime($diff["last_updated"]));
+        $beatmapID = $diff["id"];
+        $SR = $diff["difficulty_rating"];
         $difficultyName = $diff["version"];
-        $mode = $diff["mode"];
-        $status = $diff["approved"];
-        $blacklisted = 0;
-        $hasStoryboard = $diff["storyboard"];
-        $hasVideo = $diff["video"];
-
-        $query2 = $conn->prepare("SELECT * FROM blacklist WHERE UserID = ?");
-        $query2->bind_param("i", $creatorID);
-        $query2->execute();
-        $query2->store_result();
-        if ($query2->num_rows > 0) {
-            $query2->close();
-            die("No");
-        }
-        $query2->close();
-
-        $query3 = $conn->prepare("SELECT * FROM beatmapsets WHERE SetID = ?");
-        $query3->bind_param("i", $setID);
-        $query3->execute();
-        $query3->store_result();
-        if ($query3->num_rows == 0) {
-            $beatmapset_stmt->execute();
-        }
+        $mode = $diff["mode_int"];
 
         $beatmap_stmt->execute();
-        $creators_stmt->execute();
+
+        $owners = !empty($diff["owners"]) ? $diff["owners"] : [["id" => $diff["user_id"]]];
+        foreach ($owners as $owner) {
+            $diffCreatorID = $owner["id"];
+            $creators_stmt->execute();
+        }
     }
 
     header("Location: ../../mapset/" . $setID);
