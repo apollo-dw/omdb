bc678a26d78e3cfe9bc73033c32b520f88564b32
VINXIS <oykxf2@gmail.com>
2026-06-07 18:05:06 -0600

    get beatmapset apiv2 func

--- functions.php
+++ functions.php
@@ -42,7 +42,28 @@
 
 		return json_decode($response, true);
 	}
-	
+
+	function GetBeatmapsetDataOsuApi(string $token, int $id){
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => 'https://osu.ppy.sh/api/v2/beatmapsets/' . strval($id),
+		  CURLOPT_HTTPHEADER => ['Accept: application/json', 'Content-Type: application/json', 'Authorization: Bearer ' . $token],
+		  CURLOPT_RETURNTRANSFER => true,
+	      CURLOPT_ENCODING => '',
+	      CURLOPT_MAXREDIRS => 10,
+	      CURLOPT_TIMEOUT => 0,
+	      CURLOPT_FOLLOWLOCATION => true,
+	      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	      CURLOPT_CUSTOMREQUEST => 'GET',
+		));
+
+		$response = curl_exec($curl);
+		curl_close($curl);
+
+		return json_decode($response, true);
+	}
+
 	function GetOwnUserData(string $token){
 		$curl = curl_init();
 	
