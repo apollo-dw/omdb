17f6688772521a05049dfe631c7af351351b57e8
Apollo <js1086@student.le.ac.uk>
2026-07-17 17:12:50 +0100

    add static patronage page

---
+++ patron/index.php
@@ -0,0 +1,80 @@
+<?php
+    $PageTitle = "Patron";
+    require "../base.php";
+    require "../header.php";
+?>
+
+<div style="width:100%;text-align:center">
+    <h1>OMDB Patron</h1>
+</div>
+
+<div class="flex-container column-when-mobile-container" style="align-items: center;">
+    <div class="flex-child column-when-mobile" style="width:30%; text-align: center;">
+        <img src="../../assets/img/omdb-192x192.png" alt="OMDB logo"/>
+    </div>
+    <div class="flex-child column-when-mobile" style="width:60%;">
+        <div style="background-color: darkslategray;padding:1em;">
+            <p>
+                You can become an OMDB Patron to help support the site! Your contributions will go directly towards the site: server upkeep, upgrades to infrastructure, and helping keeping my lights on. My lights and nothing else.
+            </p>
+            <p>
+                Whenever I mention that I own and manage a site with this many people, I often hear "you should put advertisements on it!". Well, that's never gonna happen. I would never do that to any of you. This site will forever be free to use without any advertisement, but to anyone who wishes to support the site somehow then this option is available. Thank you so much if you choose to do this!
+            </p>
+        </div>
+    </div>  
+</div>
+
+<hr>
+
+<div style="width:100%;text-align:center;margin-top:5em;margin-bottom:5em;color: darkslategrey;background-color: white;">
+    <p style="padding:2em;">
+        <b>OMDB patronage is currently in development and is unavailable for now</b> <br>
+    </p>
+</div>
+
+
+<hr>
+
+<div style="width:100%;text-align:center">
+    <h2>Benefits</h2>
+</div>
+
+
+<div class="flex-container" style="align-items: center; flex-direction: column;">
+    <div class="alternating-bg" style="width: 50%; text-align: center;">
+        <p>
+            <b>Badge</b> <br>
+            Display a badge on your profile and comments to show your support
+        </p>
+    </div>
+    <div class="alternating-bg" style="width: 50%; text-align: center;">
+        <p>
+            <b>Profile theme</b> <br>
+            Customize the colour theme of your profile for anyone visiting
+        </p>
+    </div>
+    <div class="alternating-bg" style="width: 50%; text-align: center;">
+        <p>
+            <b>Discord role</b> <br>
+            Get a distinctly <i>DarkSlateGrey-ish</i> role on the OMDB Discord.
+        </p>
+    </div>
+    <div class="alternating-bg" style="width: 50%; text-align: center;">
+        <p>
+            <b>Shoutout on this page</b> <br>
+            Get shouted out on this page for your contributions (present and past!)
+        </p>
+    </div>
+    <div class="alternating-bg" style="width: 50%; text-align: center;">
+        <p>
+            <b>... and maybe more!</b> <br>
+            As this patron service continues, more benefits will be added
+        </p>
+    </div>
+</div>
+
+
+
+<?php
+    require '../footer.php'
+?>
