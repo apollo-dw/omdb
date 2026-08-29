7e82ce34175e45b87f5e6b9cbbf98feea8f6d1bc
Apollo <js1086@student.le.ac.uk>
2026-08-29 16:26:33 +0100

    Add some OMDB statistics to labs page

--- public/labs/stats.php
+++ public/labs/stats.php
@@ -3,10 +3,9 @@
     require '../header.php';
 ?>
 
-<h1> Stats </h1>
+<h1>Stats</h1>
 
 <hr>
-
 
 <b>Most credited mappers</b>
     <div id="credit-ranking" style="width:32em;max-width:100%;">
@@ -41,6 +40,75 @@
         }
     ?>
 </div>
+<br>
+
+<h2>OMDB ratings</h2>
+<hr>
+Average rating by year:
+<?php
+$query = "
+    SELECT
+        YEAR(bs.Timestamp) AS RatingYear,
+        COUNT(*) AS TotalRatings,
+        ROUND(AVG(r.Score), 2) AS AverageRating
+    FROM ratings r
+    JOIN beatmaps b
+        ON b.BeatmapID = r.BeatmapID
+    JOIN beatmapsets bs
+        ON bs.SetID = b.SetID
+    GROUP BY YEAR(bs.Timestamp)
+    ORDER BY RatingYear DESC;
+";
+
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    $year = (int)$row["RatingYear"];
+    $averageRating = (float)$row["AverageRating"];
+    $totalRatings = (int)$row["TotalRatings"];
+?>
+    <div style="display: flex; justify-content: space-between; width:25%;" class="subText">
+        <span><b><?php echo $year; ?></b></span>
+        <span><?php echo number_format($averageRating, 2); ?>★ (<?php echo $totalRatings; ?> rated)</span>
+    </div>
+<?php } ?>
+
+<br>
+
+Average rating by descriptor:
+<?php
+$query = "
+    SELECT
+        d.DescriptorID,
+        d.Name AS DescriptorName,
+        COUNT(*) AS TotalRatings,
+        ROUND(AVG(r.Score), 2) AS AverageRating
+    FROM ratings r
+    JOIN beatmap_descriptors bd
+        ON bd.BeatmapID = r.BeatmapID
+    JOIN descriptors d
+        ON d.DescriptorID = bd.DescriptorID
+    WHERE d.Usable = 1
+    GROUP BY d.DescriptorID, d.Name
+    ORDER BY AverageRating DESC, TotalRatings DESC;
+";
+
+$stmt = $conn->prepare($query);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    $formattedName = htmlspecialchars($row["DescriptorName"]);
+    $averageRating = (float)$row["AverageRating"];
+    $totalRatings = (int)$row["TotalRatings"];
+?>
+        <div style="display: flex; justify-content: space-between; width:25%;" class="subText">
+            <span><b><?php echo $formattedName; ?></b></span>
+            <span><?php echo number_format($averageRating, 2); ?>★ (<?php echo $totalRatings; ?> rated)</span>
+        </div>
+<?php } ?>
 
 <?php
     require "../footer.php";
